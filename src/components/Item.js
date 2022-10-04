854ffb04689771af8c023ccbfbda5b7bc607e3d3
Eric Song <ericksong91@gmail.com>
2022-10-04 19:22:33 -0400

    Done with assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [isOn, setIsOn] = useState(false);
+  // replace 'false' with a state variable that can be toggled between true and false
+  // this will be used for the Dark Mode Toggle feature
+  const appClass = isOn ? "in-cart" : ""
+  
+  function handleClick() {
+    setIsOn((isOn) => !isOn)
+    console.log(isOn)
+  }
+
   return (
-    <li className="">
+    <li className={appClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{isOn ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
